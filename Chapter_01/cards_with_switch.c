7bd0c4a0bb4ffb4bb1c64dfdf2daa5e0b86729d2
Ange TOSSOU <angetossou.dev@gmail.com>
2025-01-08 23:03:12 +0100

    Switch statements

---
+++ Chapter_01/cards_with_switch.c
@@ -0,0 +1,30 @@
+/*
+* Program to evaluate face values.
+ * Released under the Vegas Public License.
+ * (c)2014, The College Blackjack Team.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char card_name[3];
+    puts("Enter the card_name: ");
+    scanf("%2s", card_name);
+    int val = 0;
+    switch (card_name[0]) {
+        case 'K':
+        case 'Q':
+        case 'J':
+            val = 10;
+            break;
+        case 'A':
+            val = 11;
+            break;
+        default:
+            val = atoi(card_name);
+    }
+
+    return 0;
+}
