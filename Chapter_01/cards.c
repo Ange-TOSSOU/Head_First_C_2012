67d8168f288864d6e019d30a95ee7236cf17f958
Ange TOSSOU <angetossou.dev@gmail.com>
2025-01-08 11:00:36 +0100

    Update the code

--- Chapter_01/cards.c
+++ Chapter_01/cards.c
@@ -10,7 +10,7 @@
 int main()
 {
     char card_name[3];
-    puts("Enter the card name: ");
+    puts("Enter the card_name: ");
     scanf("%2s", card_name);
     int val = 0;
     if (card_name[0] == 'K') {
